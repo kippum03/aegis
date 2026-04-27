79c62da6ed23b3fc275908ab6aad58fa09000758
kippum03 <kippum03@naver.com>
2026-04-27 02:27:00 -0400

    Initial Commit

---
+++ Project-Aegis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-Aegis
+//
+//  Created by Eric Kim on 4/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-Aegis/Project_AegisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_AegisApp.swift
+//  Project-Aegis
+//
+//  Created by Eric Kim on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Project_AegisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
